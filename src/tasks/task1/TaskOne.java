10c4bfa6b13577fcfaa24446edcb0ff0525a34c4
UlybinSergej <67759732+UlybinSergej@users.noreply.github.com>
2020-10-19 18:54:34 +0300

    Update TaskOne.java

--- src/tasks/task1/TaskOne.java
+++ src/tasks/task1/TaskOne.java
@@ -9,12 +9,16 @@ public class TaskOne {
     public static void main(String[] args) throws IOException {
         Integer firstNum, secondNum;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
         System.out.println("Введите первое число:");
         firstNum = Integer.parseInt(reader.readLine());
+        
         System.out.println("Введите второе число:");
         secondNum = Integer.parseInt(reader.readLine());
+        
         if (firstNum > secondNum) System.out.println(String.format("Число %1$d больше %2$d", firstNum, secondNum));
         else if (secondNum > firstNum) System.out.println(String.format("Число %2$d больше %1$d", firstNum, secondNum));
+        
         System.out.println("Сумма равна " + (firstNum + secondNum));
     }
 
