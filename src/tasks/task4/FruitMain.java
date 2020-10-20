cbbd663427552986eb94c8285b099528bfe63fcf
sulybin <sergison95@gmail.com>
2020-10-20 15:54:54 +0300

    Task №4

---
+++ src/tasks/task4/FruitMain.java
@@ -0,0 +1,15 @@
+package tasks.task4;
+
+import java.util.Random;
+
+public class FruitMain {
+
+    public static void main(String[] args) {
+
+    }
+
+    public Fruit getRandomFruit() {
+        Random random = new Random();
+        return
+    }
+}
