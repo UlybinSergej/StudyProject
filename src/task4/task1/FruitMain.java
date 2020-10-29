dc65afb25a6d44321bd5d79ee10ab2b7c245be74
sulybin <sergison95@gmail.com>
2020-10-29 09:58:54 +0300

    Selenium Lesson 6

--- src/task4/task1/FruitMain.java
+++ src/task4/task1/FruitMain.java
@@ -1,24 +1,24 @@
 package task4.task1;
 
 import java.util.Random;
-
-public class FruitMain {
-    public static void main(String[] args) {
-        Fruit[] fruits = new Fruit[4];
-        for (int i = 0; i < 4; i++) {
-            fruits[i] = getRandomFruit();
-            System.out.println(fruits[i]);
-        }
-    }
-
-    public static Fruit getRandomFruit() {
-        Random random = new Random();
-        int randomFruit = random.nextInt(3);
-        return switch (randomFruit) {
-            case 0 -> Apple.getRandomApple();
-            case 1 -> Orange.getRandomOrange();
-            case 2 -> Pineapple.getRandomPineapple();
-            default -> null;
-        };
-    }
-}
+//
+//public class FruitMain {
+//    public static void main(String[] args) {
+//        Fruit[] fruits = new Fruit[4];
+//        for (int i = 0; i < 4; i++) {
+//            fruits[i] = getRandomFruit();
+//            System.out.println(fruits[i]);
+//        }
+//    }
+//
+////    public static Fruit getRandomFruit() {
+//        Random random = new Random();
+//        int randomFruit = random.nextInt(3);
+//        return switch (randomFruit) {
+//            case 0 -> Apple.getRandomApple();
+//            case 1 -> Orange.getRandomOrange();
+//            case 2 -> Pineapple.getRandomPineapple();
+//            default -> null;
+//        };
+//    }
+//}
